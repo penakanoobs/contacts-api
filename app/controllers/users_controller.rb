0d89e0e3aa0ea8bf0ee4be1aeebbd257c276f49b
kencruz07 <kencruz07@gmail.com>
2015-09-14 15:41:25 +0800

    Change code to hash rocket notation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      render json: {message: 'User created successfully'}
+      render json: {:message => 'User created successfully'}
     end
   end
 

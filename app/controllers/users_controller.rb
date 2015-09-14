587d3b1cc2344390cd7a84e964ba63d0c02fa422
kencruz07 <kencruz07@gmail.com>
2015-09-14 15:52:30 +0800

    Change to hash rocket, made params private, reduce routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      render json: {:message => 'User created successfully'}
+      render :json => {:message => 'User created successfully'}
     end
   end
+
+
+
+  private
 
   def user_params
     params.require(:user).permit :name, :email, :password
